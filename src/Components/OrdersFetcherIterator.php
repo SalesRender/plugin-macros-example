c5c9744359c49624c7748ac51898fa4fbdc02517
disami115 <disami115@yandex.ru>
2020-11-12 21:58:47 +0300

    Добавил OrdersFetcherIterator

---
+++ src/Components/OrdersFetcherIterator.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created for plugin-exporter-excel
+ * Datetime: 30.07.2019 16:20
+ * @author Timur Kasumov aka XAKEPEHOK
+ */
+
+namespace Leadvertex\Plugin\Instance\Macros\Components;
+
+
+use Leadvertex\Plugin\Components\ApiClient\ApiFetcherIterator;
+use XAKEPEHOK\ArrayGraphQL\ArrayGraphQL;
+use XAKEPEHOK\ArrayGraphQL\InvalidArrayException;
+
+class OrdersFetcherIterator extends ApiFetcherIterator
+{
+
+    /**
+     * @param array $body
+     * @return string
+     * @throws InvalidArrayException
+     */
+    public function getQuery(array $body): string
+    {
+        $fields = ArrayGraphQL::convert($body);
+        return '
+            query($pagination: Pagination!, $filters: OrderFilter, $sort: OrderSort) {
+                ordersFetcher(pagination: $pagination, filters: $filters, sort: $sort) ' . $fields . '
+            }
+        ';
+    }
+
+    /**
+     * Dot-notation string to query body
+     * @return string
+     */
+    protected function getQueryPath(): string
+    {
+        return 'ordersFetcher';
+    }
+
+    protected function getIdentity(array $array): string
+    {
+        return $array['id'];
+    }
+}
