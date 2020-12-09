f6d3e9797014a198d8c7d321dd5e1433914e47a8
disami115 <disami115@yandex.ru>
2020-12-09 22:11:13 +0300

    Добавил addUploadAction

--- public/index.php
+++ public/index.php
@@ -4,5 +4,6 @@ use Leadvertex\Plugin\Core\Macros\Factories\WebAppFactory;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $factory = new WebAppFactory();
+$factory->addUploadAction();
 $application = $factory->build();
 $application->run();
