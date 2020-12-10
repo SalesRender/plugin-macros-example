1f9a9ba2072c4cb074a1ee902f4be13222441df7
XAKEPEHOK <icq-475204@ya.ru>
2020-12-10 20:11:31 +0300

    DynamicValues fix

--- src/Forms/SettingsForm.php
+++ src/Forms/SettingsForm.php
@@ -318,7 +318,7 @@ class SettingsForm extends Form
                     }
                     return $errors;
                 },
-                new DynamicValues($_ENV['LV_PLUGIN_SELF_URI'] . 'autocomplete/example'),
+                new DynamicValues('example'),
                 new Limit(2, null),
                 $withDefault ? ['dynamic_10', 'dynamic_1'] : null
             ),
